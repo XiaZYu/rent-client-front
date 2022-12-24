825c27b0c34dae91f02e3e19d1d5b39ee8f4e866
Larry <351220018@qq.com>
2022-12-24 17:47:05 +0800

    更新

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        Hello, 张震杰
+        Hello, 小屁股张震杰
       </main>
     </>
   )
